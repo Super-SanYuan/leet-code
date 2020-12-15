2e37deeb917516dd1ecb0a0e9e176b2999f7fd16
sanyuan <sanyuan.jw@foxmail.com>
2020-12-15 14:05:33 +0800

    feat: 01.两数之和

---
+++ code/1.两数之和.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let res = []
+  for (let i = 0, len = nums.length; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      if (nums[i] + nums[j] === target) {
+        res.push(i, j)
+      }
+    }
+  }
+  return res
+};
+// @lc code=end
+
